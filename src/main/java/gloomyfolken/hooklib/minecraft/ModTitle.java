ec70ad3608e2822429d6189d02f3384da5027cd4
hohserg <hohserg1@yandex.ru>
2019-07-24 02:02:26 +0300

    Added face on HookLib

---
+++ src/main/java/gloomyfolken/hooklib/minecraft/ModTitle.java
@@ -0,0 +1,7 @@
+package gloomyfolken.hooklib.minecraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "hooklib", name = "HookLib", version = "1.0",acceptedMinecraftVersions = "1.12")
+public class ModTitle {
+}
