a88efa271ba27f6496bee30003e4403c914f1d3f
hohserg <hohserg1@yandex.ru>
2019-07-27 18:25:57 +0300

    Remove deprecated

--- src/main/java/gloomyfolken/hooklib/asm/HookInjectorFactory.java
+++ src/main/java/gloomyfolken/hooklib/asm/HookInjectorFactory.java
@@ -33,36 +33,6 @@ public abstract class HookInjectorFactory {
 
     }
 
-
-    static class MethodEnter extends HookInjectorFactory {
-
-        public static final MethodEnter INSTANCE = new MethodEnter();
-
-        private MethodEnter() {}
-
-        @Override
-        public HookInjectorMethodVisitor createHookInjector(MethodVisitor mv, int access, String name, String desc,
-                                                            AsmHook hook, HookInjectorClassVisitor cv) {
-            return new HookInjectorMethodVisitor.MethodEnter(mv, access, name, desc, hook, cv);
-        }
-
-    }
-
-    static class MethodExit extends HookInjectorFactory {
-
-        public static final MethodExit INSTANCE = new MethodExit();
-
-        private MethodExit() {
-            isPriorityInverted = true;
-        }
-
-        @Override
-        public HookInjectorMethodVisitor createHookInjector(MethodVisitor mv, int access, String name, String desc,
-                                                            AsmHook hook, HookInjectorClassVisitor cv) {
-            return new HookInjectorMethodVisitor.MethodExit(mv, access, name, desc, hook, cv);
-        }
-    }
-
     static class LineNumber extends HookInjectorFactory {
 
         private int lineNumber;
