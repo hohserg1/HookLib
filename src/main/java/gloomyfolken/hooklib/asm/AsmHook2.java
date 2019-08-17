89be8156324f8ada5f58e676856e906ef893288a
hohserg <hohserg1@yandex.ru>
2019-08-18 00:47:05 +0300

    Update AsmHook2.java

--- src/main/java/gloomyfolken/hooklib/asm/AsmHook2.java
+++ src/main/java/gloomyfolken/hooklib/asm/AsmHook2.java
@@ -1,4 +1,25 @@
 package gloomyfolken.hooklib.asm;
 
-public class AsmHook2 {
+public class AsmHook2 implements Comparable<AsmHook> {
+    public final TargetMethodSpec targetMethod;
+    public final HookSpec hookMethod;
+
+    public final boolean createMethod;
+    public final boolean isMandatory;
+    public final ReturnCondition returnCondition;
+    public final HookPriority priority;
+
+    public final Anchor anchor;
+
+    @Override
+    public int compareTo(AsmHook o) {
+        if (anchor.point.isPriorityInverted && o.anchor.point.isPriorityInverted) {
+            return priority.ordinal() > o.priority.ordinal() ? -1 : 1;
+        } else if (!anchor.point.isPriorityInverted && !o.anchor.point.isPriorityInverted) {
+            return priority.ordinal() > o.priority.ordinal() ? 1 : -1;
+        } else {
+            return anchor.point.isPriorityInverted ? 1 : -1;
+        }
+    }
 }
+
