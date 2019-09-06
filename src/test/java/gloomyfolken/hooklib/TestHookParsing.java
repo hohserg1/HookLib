9272f73f951d68c654b3474b40731273ef2e2226
hohserg <hohserg1@yandex.ru>
2019-09-06 19:36:40 +0300

    Added tests for hook parsing

---
+++ src/test/java/gloomyfolken/hooklib/TestHookParsing.java
@@ -0,0 +1,216 @@
+package gloomyfolken.hooklib;
+
+import com.google.common.collect.ImmutableList;
+import gloomyfolken.hooklib.asm.*;
+import gloomyfolken.hooklib.asm.model.AsmHook;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityHopper;
+import net.minecraft.util.EnumFacing;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.Type;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestHookParsing {
+    HookClassTransformer transformer;
+    public static final int hooksCount = 7;
+    private List<AsmHook> testHooks;
+    private List<AsmHook> handmadeHooks;
+
+    @Before
+    public void init() {
+        transformer = new HookClassTransformer();
+
+        testHooks = transformer.containerParser.parseHooks("gloomyfolken.hooklib.example.TestHooks").collect(Collectors.toList());
+
+        handmadeHooks = ImmutableList.of(
+                AsmHook.builder()
+                        .targetClassName("net.minecraft.item.Item$ToolMaterial")
+                        .targetMethodName("<init>")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("toolMaterial")
+
+                        .startArgumentsFill()
+                        .addTargetMethodParameter(Type.getType(String.class))
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.FLOAT_TYPE)
+                        .addTargetMethodParameter(Type.FLOAT_TYPE)
+                        .addTargetMethodParameter(Type.INT_TYPE)
+
+                        .addThisToHookMethodParameters()
+                        .addHookMethodParameter(Type.getType(String.class), 1)
+                        .addHookMethodParameter(Type.INT_TYPE, 2)
+                        .addHookMethodParameter(Type.INT_TYPE, 3)
+                        .addHookMethodParameter(Type.INT_TYPE, 4)
+                        .addHookMethodParameter(Type.FLOAT_TYPE, 5)
+                        .addHookMethodParameter(Type.FLOAT_TYPE, 6)
+                        .addHookMethodParameter(Type.INT_TYPE, 7)
+
+                        .hookMethodReturnType(Type.VOID_TYPE)
+
+                        .finishArgumentsFill()
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("gloomyfolken.hooklib.example.TestHooks")
+                        .targetMethodName("insertStack")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("insertStack")
+
+                        .startArgumentsFill()
+                        .addTargetMethodParameter(Type.getType(TileEntityHopper.class))
+                        .addTargetMethodParameter(Type.getType(IInventory.class))
+                        .addTargetMethodParameter(Type.getType(IInventory.class))
+                        .addTargetMethodParameter(Type.getType(ItemStack.class))
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.getType(EnumFacing.class))
+
+                        .addThisToHookMethodParameters()
+                        .addHookMethodParameter(Type.getType(TileEntityHopper.class), 1)
+                        .addHookMethodParameter(Type.getType(IInventory.class), 2)
+                        .addHookMethodParameter(Type.getType(IInventory.class), 3)
+                        .addHookMethodParameter(Type.getType(ItemStack.class), 4)
+                        .addHookMethodParameter(Type.INT_TYPE, 5)
+                        .addHookMethodParameter(Type.getType(EnumFacing.class), 6)
+
+                        .hookMethodReturnType(Type.VOID_TYPE)
+
+                        .finishArgumentsFill()
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("net.minecraft.tileentity.TileEntityHopper")
+                        .targetMethodName("insertStack")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("insertStack")
+                        .hookMethodReturnType(Type.VOID_TYPE)
+
+                        .startArgumentsFill()
+                        .addTargetMethodParameter(Type.getType(IInventory.class))
+                        .addTargetMethodParameter(Type.getType(IInventory.class))
+                        .addTargetMethodParameter(Type.getType(ItemStack.class))
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.getType(EnumFacing.class))
+
+                        .addThisToHookMethodParameters()
+                        .addHookMethodParameter(Type.getType(IInventory.class), 1)
+                        .addHookMethodParameter(Type.getType(IInventory.class), 2)
+                        .addHookMethodParameter(Type.getType(ItemStack.class), 3)
+                        .addHookMethodParameter(Type.INT_TYPE, 4)
+                        .addHookMethodParameter(Type.getType(EnumFacing.class), 5)
+
+
+                        .finishArgumentsFill()
+
+                        .point(InjectionPoint.METHOD_CALL)
+                        .shift(Shift.INSTEAD)
+                        .anchorTarget("setInventorySlotContents")
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("net.minecraft.client.Minecraft")
+                        .targetMethodName("resize")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("resize")
+                        .hookMethodReturnType(Type.VOID_TYPE)
+
+                        .startArgumentsFill()
+                        .addTargetMethodParameter(Type.INT_TYPE)
+                        .addTargetMethodParameter(Type.INT_TYPE)
+
+                        .addThisToHookMethodParameters()
+                        .addHookMethodParameter(Type.INT_TYPE, 1)
+                        .addHookMethodParameter(Type.INT_TYPE, 2)
+
+
+                        .finishArgumentsFill()
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("net.minecraftforge.common.ForgeHooks")
+                        .targetMethodName("getTotalArmorValue")
+                        .targetMethodReturnType(Type.INT_TYPE)
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("getTotalArmorValue")
+                        .hookMethodReturnType(Type.INT_TYPE)
+
+                        .startArgumentsFill()
+                        .addTargetMethodParameter(Type.getType(EntityPlayer.class))
+
+                        .addThisToHookMethodParameters()
+                        .addHookMethodParameter(Type.getType(EntityPlayer.class), 1)
+                        .addReturnValueToHookMethodParameters()
+
+                        .finishArgumentsFill()
+
+                        .returnCondition(ReturnCondition.ALWAYS)
+                        .point(InjectionPoint.RETURN)
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("net.minecraft.entity.player.EntityPlayer")
+                        .targetMethodName("getPortalCooldown")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("getPortalCooldown")
+                        .hookMethodReturnType(Type.getType(ResultSolve.class))
+
+                        .startArgumentsFill()
+
+                        .addThisToHookMethodParameters()
+
+                        .finishArgumentsFill()
+
+                        .returnCondition(ReturnCondition.ON_SOLVE)
+                        .build(),
+
+                AsmHook.builder()
+                        .targetClassName("net.minecraft.entity.Entity")
+                        .targetMethodName("getBrightnessForRender")
+
+                        .hookClassInternalName("gloomyfolken/hooklib/example/TestHooks")
+                        .hookMethodName("getBrightnessForRender")
+                        .hookMethodReturnType(Type.getType(ResultSolve.class))
+
+                        .startArgumentsFill()
+
+                        .addThisToHookMethodParameters()
+
+                        .finishArgumentsFill()
+
+                        .returnCondition(ReturnCondition.ON_SOLVE)
+                        .build()
+
+        );
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAsmHookParsing() {
+        for (int i = 0; i < testHooks.size(); i++) {
+            AsmHook parsedHook = testHooks.get(i);
+            AsmHook handmadeHook = handmadeHooks.get(i);
+            assertEquals("hook number id " + i, handmadeHook, parsedHook);
+        }
+
+    }
+
+}
