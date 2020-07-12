e759d93dd243dfa174f55448cef6913ae3ba9893
hohserg <hohserg1@yandex.ru>
2020-07-12 16:56:39 +0300

    Added space

--- src/main/java/gloomyfolken/hooklib/minecraft/SecondaryTransformerHook.java
+++ src/main/java/gloomyfolken/hooklib/minecraft/SecondaryTransformerHook.java
@@ -15,7 +15,7 @@ public class SecondaryTransformerHook {
     public static void injectData(Loader loader, Object... data) {
         ClassLoader classLoader = SecondaryTransformerHook.class.getClassLoader();
         if (classLoader instanceof LaunchClassLoader) {
-            ((LaunchClassLoader)classLoader).registerTransformer(MinecraftClassTransformer.class.getName());
+            ((LaunchClassLoader) classLoader).registerTransformer(MinecraftClassTransformer.class.getName());
         } else {
             System.out.println("HookLib was not loaded by LaunchClassLoader. Hooks will not be injected.");
         }
