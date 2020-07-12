bab79efec0021402733a0041d900335b80ef2002
hohserg <hohserg1@yandex.ru>
2020-07-12 21:08:34 +0300

    Clear project
    Removed a little less than all

--- src/main/java/gloomyfolken/hooklib/asm/HookLens.java
+++
@@ -1,9 +0,0 @@
-package gloomyfolken.hooklib.asm;
-
-public @interface HookLens {
-    public String name() default "";
-
-    public boolean boxing() default false;
-
-    public boolean createField() default false;
-}
--- src/main/java/gloomyfolken/hooklib/asm/Lens.java
+++
@@ -1,9 +0,0 @@
-package gloomyfolken.hooklib.asm;
-
-public abstract class Lens<A, B> {
-
-    public abstract void set(A a, B b);
-
-    public abstract B get(A a);
-
-}
