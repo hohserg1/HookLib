932af10147f7bec7ec94282e2f24fc7411d48d2d
hohserg <hohserg1@yandex.ru>
2020-07-14 21:47:47 +0300

    Added re-adding transformer for take deobf method names

--- src/main/java/gloomyfolken/hooklib/minecraft/ScannerClassTransformer.java
+++ src/main/java/gloomyfolken/hooklib/minecraft/ScannerClassTransformer.java
@@ -1,16 +1,45 @@
 package gloomyfolken.hooklib.minecraft;
 
+import gloomyfolken.hooklib.api.At;
+import gloomyfolken.hooklib.api.Hook;
+import gloomyfolken.hooklib.api.InjectionPoint;
 import gloomyfolken.hooklib.common.SafeClassWriter;
 import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.fml.common.Loader;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.objectweb.asm.Opcodes.ASM5;
 
 public class ScannerClassTransformer implements IClassTransformer {
+
+    private static ScannerClassTransformer instance;
+
+    public ScannerClassTransformer() {
+        instance = this;
+    }
+
+    @Hook(at = @At(point = InjectionPoint.HEAD))
+    public static void injectData(Loader loader, Object... data) {
+        ClassLoader classLoader = ScannerClassTransformer.class.getClassLoader();
+        if (classLoader instanceof LaunchClassLoader) {
+            try {
+                List<IClassTransformer> transformers = (List<IClassTransformer>) LaunchClassLoader.class.getDeclaredField("transformers").get(classLoader);
+                transformers.remove(instance);//Помещаем трансформер хуклибы в конец списка, чтобы он применялся после DeobfuscationTransformer
+                transformers.add(instance);
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("HookLib was not loaded by LaunchClassLoader. Hooks will not be injected.");
+        }
+    }
+
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         System.out.println("transform " + name.equals(transformedName) + " name " + name + ", transformedName " + transformedName);
