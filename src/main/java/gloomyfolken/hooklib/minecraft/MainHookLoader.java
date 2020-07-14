e75d7921e083fc0cdc4f3ec2e9ca829cedefe469
hohserg <hohserg1@yandex.ru>
2020-07-14 18:22:48 +0300

    Added feature auto preinit classes which used in transformer

--- src/main/java/gloomyfolken/hooklib/minecraft/MainHookLoader.java
+++ src/main/java/gloomyfolken/hooklib/minecraft/MainHookLoader.java
@@ -1,11 +1,65 @@
 package gloomyfolken.hooklib.minecraft;
 
+import gloomyfolken.hooklib.common.AsmHelper;
+import gloomyfolken.hooklib.common.ClassMetadataReader;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.objectweb.asm.tree.AbstractInsnNode;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class MainHookLoader implements IFMLLoadingPlugin {
+
+    public MainHookLoader() {
+        putClassesToFML();
+        //System.out.println(DeobfHelper.class);
+
+    }
+
+    private void putClassesToFML() {
+        Arrays.stream(getASMTransformerClass())
+                .flatMap(className -> {
+                    try {
+                        return Stream.of(ClassMetadataReader.instance.getClassData(className));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return Stream.empty();
+                    }
+                })
+                .map(AsmHelper::classNodeOf)
+                .flatMap(cn ->
+                        cn.methods.stream().flatMap(mn -> {
+                            Stream<AbstractInsnNode> instructions = StreamSupport.stream(
+                                    Spliterators.spliteratorUnknownSize(mn.instructions.iterator(), Spliterator.ORDERED),
+                                    false);
+                            return instructions.flatMap(in -> {
+                                try {
+                                    return Stream.of((String) in.getClass().getDeclaredField("owner").get(in));
+                                } catch (NoSuchFieldException | IllegalAccessException e) {
+                                    return Stream.empty();
+                                }
+                            });
+                        })
+                )
+                .map(claccName -> claccName.replace('/', '.'))
+                .collect(Collectors.toSet())
+                .forEach(className -> {
+                    try {
+                        System.out.println("Preloaded for HookLib transformer " + className);
+                        Class.forName(className);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
     @Override
     public String[] getASMTransformerClass() {
         return new String[]{ScannerClassTransformer.class.getName()};
