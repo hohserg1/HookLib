a50c7cc1fd3e63ce15b488aefc41989106ec0c7a
hohserg <hohserg1@yandex.ru>
2020-07-16 19:25:11 +0300

    Added test hook for test parsing

--- src/main/java/gloomyfolken/hooklib/example/TestHooks.java
+++ src/main/java/gloomyfolken/hooklib/example/TestHooks.java
@@ -1,11 +1,20 @@
 package gloomyfolken.hooklib.example;
 
+import gloomyfolken.hooklib.api.Hook;
 import gloomyfolken.hooklib.api.HookContainer;
 import gloomyfolken.hooklib.api.HookLens;
 import gloomyfolken.hooklib.api.Lens;
+import net.minecraft.tileentity.TileEntity;
 
 @HookContainer(modid = "test-hooklib")
 public class TestHooks {
+
+    @Hook
+    public static void test(TileEntity tileEntity, int a, @Hook.LocalVariable(2) int b, int c) {
+
+    }
+
+
     @HookLens
     public static Lens<Test, String> test = new Lens<Test, String>() {
     };
