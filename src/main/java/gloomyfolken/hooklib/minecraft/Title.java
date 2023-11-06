84be6bb64284035948271977484db296ae421e5d
hohserg1 <hohserg1@yandex.ru>
2023-11-06 17:21:54 +0300

    Allow to use hooklib at server without have at client

--- src/main/java/gloomyfolken/hooklib/minecraft/Title.java
+++ src/main/java/gloomyfolken/hooklib/minecraft/Title.java
@@ -2,6 +2,6 @@ package gloomyfolken.hooklib.minecraft;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "hooklib")
+@Mod(modid = "hooklib", acceptableRemoteVersions = "*")
 public class Title {
 }
