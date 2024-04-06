abc8bc25bbdcac3a67fee6daf3ac4d2baf9ee738
hohserg1 <hohserg1@yandex.ru>
2024-04-06 06:21:24 +0300

    improved ReturnSolve api

--- src/main/java/gloomyfolken/hooklib/api/ReturnSolve.java
+++ src/main/java/gloomyfolken/hooklib/api/ReturnSolve.java
@@ -1,5 +1,13 @@
 package gloomyfolken.hooklib.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Possible return type of hook-method, which allows a hook-method to determine whether a target-method needs to do return
+ *
+ * @param <A> return type of target method, can be marked by {@link Primitive} annotation for strictly primitive type
+ */
 public interface ReturnSolve<A> {
     static <A> ReturnSolve<A> yes(A value) {
         return new Yes<>(value);
@@ -19,4 +27,13 @@ public interface ReturnSolve<A> {
 
     static ReturnSolve no = new ReturnSolve() {
     };
+
+    /**
+     * Type parameter of {@link ReturnSolve} can be marked by this annotation
+     * <p>
+     * Have sense only with {@link Hook#createMethod}, otherwise HookLib will try to find target method with primitive return type anyway
+     */
+    @Target(ElementType.TYPE_USE)
+    @interface Primitive {
+    }
 }
