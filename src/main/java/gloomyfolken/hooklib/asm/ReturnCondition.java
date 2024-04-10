6e103590f9e3fb409c5c03bdf5fe49c71e676cf5
hohserg1 <hohserg1@yandex.ru>
2024-04-10 13:06:55 +0300

    simplify

--- src/main/java/gloomyfolken/hooklib/asm/ReturnCondition.java
+++ src/main/java/gloomyfolken/hooklib/asm/ReturnCondition.java
@@ -2,17 +2,7 @@ package gloomyfolken.hooklib.asm;
 
 
 public enum ReturnCondition {
-
-    NEVER(false),
-
-    ALWAYS(false),
-
-    ON_SOLVE(true);
-
-    public final boolean requiresCondition;
-
-    ReturnCondition(boolean requiresCondition) {
-        this.requiresCondition = requiresCondition;
-    }
-
+    NEVER,
+    ALWAYS,
+    ON_SOLVE
 }
