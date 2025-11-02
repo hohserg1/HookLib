4f62add9fd4038045ef883ead70087d68d1472c7
hohserg1 <hohserg1@yandex.ru>
2025-11-02 20:11:07 +0300

    boxed should not be casted to primitives

--- src/main/java/gloomyfolken/hooklib/asm/HookContainerParser.java
+++ src/main/java/gloomyfolken/hooklib/asm/HookContainerParser.java
@@ -2,18 +2,25 @@ package gloomyfolken.hooklib.asm;
 
 import com.google.common.collect.*;
 import gloomyfolken.hooklib.api.*;
-import gloomyfolken.hooklib.asm.AsmUtils.*;
-import gloomyfolken.hooklib.asm.SignatureExtractor.*;
+import gloomyfolken.hooklib.asm.AsmUtils.OpcodeDetails;
+import gloomyfolken.hooklib.asm.SignatureExtractor.FlatTypeRepr;
+import gloomyfolken.hooklib.asm.SignatureExtractor.ParametrizedTypeRepr;
+import gloomyfolken.hooklib.asm.SignatureExtractor.TypeRepr;
 import gloomyfolken.hooklib.asm.injections.*;
-import gloomyfolken.hooklib.helper.*;
-import gloomyfolken.hooklib.helper.annotation.*;
-import gloomyfolken.hooklib.minecraft.*;
-import org.objectweb.asm.*;
+import gloomyfolken.hooklib.helper.Logger;
+import gloomyfolken.hooklib.helper.SideOnlyUtils;
+import gloomyfolken.hooklib.helper.annotation.AnnotationMap;
+import gloomyfolken.hooklib.helper.annotation.AnnotationUtils;
+import gloomyfolken.hooklib.minecraft.HookLoader;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.TypePath;
+import org.objectweb.asm.TypeReference;
 import org.objectweb.asm.tree.*;
 
 import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.objectweb.asm.Opcodes.*;
 
@@ -259,9 +266,8 @@ public class HookContainerParser {
     }
 
     private boolean isSubtype(Type parent, Type some) {
-        System.out.println("isSubtype " + parent + " " + some);
-        if (AsmUtils.allPrimitives.contains(parent) || AsmUtils.allPrimitives.contains(some))
-            return AsmUtils.objectToPrimitive.getOrDefault(parent, parent).equals(AsmUtils.objectToPrimitive.getOrDefault(some, some));
+        if ((AsmUtils.allPrimitives.contains(parent) || AsmUtils.allPrimitives.contains(some)) && !parent.equals(some))
+            return false;
         return HookLoader.getDeobfuscationMetadataReader().checkSuperType(some.getInternalName(), parent.getInternalName());
     }
 
